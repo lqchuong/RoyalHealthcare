80f2b6d772b2da076aa962729f7d8736c7bf3775
Carwyn <lqbao.edu@gmail.com>
2022-04-14 01:30:04 +0700

    add register_models
    (cherry picked from commit 5ae0c57a9049598f428c35fce9821823b074521e)

---
+++ lib/app/modules/register/models/register_model.dart
@@ -0,0 +1,32 @@
+class RegisterModel {
+  final String CusName;
+  final String ReName;
+  final String CusPhoneNumber;
+  final String RePhoneNumberOther;
+  final String Gender;
+  final String DateOfBirth;
+  final String Address;
+  final String Mail;
+  final String HealthPackage;
+  final String Doctor;
+  final String CheckingType;
+  final String DateChecking;
+  final String TimeChecking;
+  final String Note;
+
+  const RegisterModel(
+      {required this.CusName,
+      required this.ReName,
+      required this.CusPhoneNumber,
+      required this.RePhoneNumberOther,
+      required this.Gender,
+      required this.DateOfBirth,
+      required this.Address,
+      required this.Mail,
+      required this.HealthPackage,
+      required this.Doctor,
+      required this.CheckingType,
+      required this.DateChecking,
+      required this.TimeChecking,
+      required this.Note});
+}
